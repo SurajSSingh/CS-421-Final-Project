55b81d79d9908f1214062743a0a257d449db768a
SurajSSingh <surajss2@illinois.edu>
2022-07-17 22:13:08 -0700

    Added Help Text to REPL

--- HaskellPSST/src/PSST/Runtime.hs
+++ HaskellPSST/src/PSST/Runtime.hs
@@ -20,6 +20,18 @@ readString = putStr replPrompt
         >> hFlush stdout
         >> getLine
 
+helpText :: [String]
+helpText = ["Welcome to String Solver"
+            , "Commands:"
+            , "  * help:              Display this help text"
+            , "  * check <variable?>: Check the current state of the solver to determine if it is satisfiable."
+            , "                       If provided with an optional variable, it will do an internal check of that variable "
+            , "                       (i.e. is the variable by itself satisfiable, assuming all other variables are?)"
+            , "  * clear <variable?>: Clears the current state of the solver. If provided with an optional variable, "
+            , "                       it will only clear that variable."
+            , ""
+            ]
+
 repl :: IO () -> Env -> IO ()
 repl main env = do
     input <- readString
@@ -28,7 +40,7 @@ repl main env = do
             print "Closing Solver"
             return ()
         help | help == "help" -> do
-            print "HELP:"
+            mapM_ putStrLn helpText
             repl main env
         _ -> case strSolParse input of
                 Right exp ->
